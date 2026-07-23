0fb08270a0921dc3f0830426f4f59631c8d1aede
Nguyen Khanh Toan <toan.nk292006@gmail.com>
2026-07-23 12:22:46 +0700

    nhap xuat

---
+++ nhapxuat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float a = 404.2402402;
+    float b = 42348.432;
+    float c,d,e;
+    cin>>c>>d>>e;
+
+    cout<<fixed << setprecision(2)<<a<<"\n" <<b<<"\n"<<c<<"\n"<<d<<"\n"<<e;
+    
+}
